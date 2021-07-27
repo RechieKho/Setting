8ee0c24fa4f9951b91d8871c8dc5f4901525ea85
RechieKho <the.real.richie.kho@gmail.com>
2021-07-27 14:36:59 +0800

    Add install_plug.sh script to make installing vim-plug for neovim on Unix, Linux more easy

---
+++ install_plug.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# install vim-plug for neovim on Unix, Linux 
+# website: github.com/junegunn/vim-plug
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
